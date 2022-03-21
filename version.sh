7ed85776158c69f6536886a649ca763c12e35ae8
igor.petrenko <igor.petrenko@xenoss.io>
2022-03-21 16:46:38 +0200

    fix version

--- version.sh
+++ version.sh
@@ -1,4 +1,6 @@
+BUILD_COUNTER=$1
+
 tcaVersion=$(grep -Po 'ENV\s+VERSION\s+\K[^\r\n]+' Dockerfile)
-tcaVersion=${tcaVersion}-%build.counter%
+tcaVersion=${tcaVersion}-${BUILD_COUNTER}
 echo "version ${tcaVersion}"
 printf "##teamcity[buildNumber '%s']\n" ${tcaVersion}
